4b167e866c3aecc72e7b81ada44426b8596e6057
Djouma Djouma <224djouma224@gmail.com>
2025-08-29 16:54:35 -0700

    modification de la description au niveau du produit

--- database/migrations/2025_08_26_192726_add_description_table_produit.php
+++ database/migrations/2025_08_26_192726_add_description_table_produit.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('produit', function (Blueprint $table) {
+        Schema::table('produits', function (Blueprint $table) {
             //
             $table->string('description')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('produit', function (Blueprint $table) {
+        Schema::table('produits', function (Blueprint $table) {
             //
 
             // supposons qu'on voulais supprimer la colonne description
